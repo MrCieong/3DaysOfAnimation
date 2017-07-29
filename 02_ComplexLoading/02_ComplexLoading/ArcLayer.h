e9822e1eb74fde7c4baaa0a9b0f58fa625d6a504
MrCieong <zhangjing0829@gmail.com>
2017-07-29 17:14:08 +0800

    MOD: 02_complexLoading

---
+++ 02_ComplexLoading/02_ComplexLoading/ArcLayer.h
@@ -0,0 +1,13 @@
+//
+//  ArcLayer.h
+//  02_ComplexLoading
+//
+//  Created by zhangjing on 2017/7/29.
+//  Copyright © 2017年 MrCieong. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface ArcLayer : CAShapeLayer
+
+@end
---
+++ 02_ComplexLoading/02_ComplexLoading/ArcLayer.m
@@ -0,0 +1,13 @@
+//
+//  ArcLayer.m
+//  02_ComplexLoading
+//
+//  Created by zhangjing on 2017/7/29.
+//  Copyright © 2017年 MrCieong. All rights reserved.
+//
+
+#import "ArcLayer.h"
+
+@implementation ArcLayer
+
+@end
